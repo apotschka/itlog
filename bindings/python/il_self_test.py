c70fde2c5f1fe2ae0defff8dbeedae443a5c4280
Andreas Potschka <potschka@iwr.uni-heidelberg.de>
2017-02-02 21:34:04 +0100

    Problem: CMake and autotools generate libraries with different names.
    Solution: Use linkname instead of libname in project.xml.

--- bindings/python/il_self_test.py
+++ bindings/python/il_self_test.py
@@ -1,16 +1,16 @@
-import itlog
+from itlog import IlLog
 
-log = itlog.IlLog()
+log = IlLog()
 
 data_max = 30.0;
 data = 0.0;
 while data < data_max:
-    log.entry(0, "%8s", "last", "%8.0f", data, itlog.IlLog.USE_LAST)
-    log.entry(0, "%8s", "average", "%8.1f", data, itlog.IlLog.USE_LAST)
-    log.entry(0, "%8s", "minimum", "%8.0f", data, itlog.IlLog.USE_LAST)
-    log.entry(0, "%8s", "maximum", "%8.0f", data, itlog.IlLog.USE_LAST)
-    log.entry(0, "%11s", "[0,1]", "%8.2e", data / (data_max - 1), \
-            itlog.IlLog.USE_LAST | itlog.IlLog.UNIT_INTERVAL)
+    log.entry(0, "%8s", "last", "%8.0f", data, IlLog.USE_LAST)
+    log.entry(0, "%8s", "average", "%8.1f", data, IlLog.USE_LAST)
+    log.entry(0, "%8s", "minimum", "%8.0f", data, IlLog.USE_LAST)
+    log.entry(0, "%8s", "maximum", "%8.0f", data, IlLog.USE_LAST)
+    log.entry(0, "%11s", "[0,1]", "%8.2e", data / (data_max - 1.0), \
+            IlLog.USE_LAST | IlLog.UNIT_INTERVAL)
     log.output_line()
     data += 1.0
 
